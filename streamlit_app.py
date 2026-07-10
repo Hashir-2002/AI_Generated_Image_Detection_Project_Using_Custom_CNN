f954af2db6304d60b36fac712f01e1b1785484f5
Hashir-2002 <146970360+Hashir-2002@users.noreply.github.com>
2026-07-10 15:02:46 +0500

    commit all

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
-import tflite_runtime.interpreter as tflite
+import tensorflow as tf
+tflite = tf.lite
 
 # Load TFLite model
 interpreter = tflite.Interpreter(model_path='ai_image_detector.tflite')
